07b4c119ede66f858a11cc183cf30f93d370d11b
ADMIN <ADMIN@NGUYENNTTD01427>
2026-02-02 23:42:41 +0700

    1_Tạo base dự án

---
+++ src/SportStore.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ src/SportStore.Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Tên loại sản phẩm không được bỏ trống!")]
+        [MaxLength(30, ErrorMessage = "Tên loại sản phẩm quá dài!")]
+        [DisplayName("Tên danh mục")]
+        public required string Name { get; set; }
+
+
+        [Range(1, 100, ErrorMessage = "Thứ tự hiển thị phải nằm trong khoảng 1 - 100!")]
+        public int DisplayOrder { get; set; }
+    }
+}
